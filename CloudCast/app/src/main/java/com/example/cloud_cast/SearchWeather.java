2511d4889cb8b7c24f183bdb511cb0c055ed470e
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-10-15 11:18:51 -0500

    Added initial search activity

---
+++ CloudCast/app/src/main/java/com/example/cloud_cast/SearchWeather.java
@@ -0,0 +1,14 @@
+package com.example.cloud_cast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchWeather extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_weather);
+    }
+}
