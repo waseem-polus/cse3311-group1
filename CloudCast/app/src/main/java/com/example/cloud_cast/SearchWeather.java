16eb21ea2cb2dc17a4e73145303712f386404808
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-10-15 11:26:12 -0500

    Added testing comment

--- CloudCast/app/src/main/java/com/example/cloud_cast/SearchWeather.java
+++ CloudCast/app/src/main/java/com/example/cloud_cast/SearchWeather.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SearchWeather extends AppCompatActivity {
-
+    //Christopher Nguyen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
