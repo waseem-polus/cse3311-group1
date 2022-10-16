14b45946b345caf5a51362d94bb5093f298355af
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-10-16 00:18:14 -0500

    Enriched the reycler view by a little bit. Still needs actually searching. Personal Note: Read TODOs for SearchWeather

--- CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
+++ CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cloud_cast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button placeHolderButton = (Button) findViewById(R.id.ChrisPlaceHolderButton);
+        placeHolderButton.setOnClickListener(v -> {
+            // Code here executes on main thread after user presses button
+            Intent intent = new Intent(v.getContext(), SearchWeather.class);
+            startActivity(intent);
+        });
     }
+
+
 }
--- CloudCast/app/src/main/java/com/example/cloud_cast/SearchWeather.java
+++ CloudCast/app/src/main/java/com/example/cloud_cast/SearchWeather.java
@@ -1,6 +1,9 @@
 package com.example.cloud_cast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -8,14 +11,32 @@ import java.util.ArrayList;
 
 public class SearchWeather extends AppCompatActivity {
     private ArrayList<SearchCityInfo> citiesList;
+    private RecyclerView recyclerView;
     //Christopher Nguyen
     //TODO: Make an actual search function (somehow...)
+    //TODO: Make the element xml function actually look better
+    //TODO: API Wizardry in tandem with the searching
+    //TODO: Update my lame deefault profile picture
+    //TODO: Finish all the Stormblood, Shadowbringers, and Endwalker Sidequests by the end of the final finals this semester
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_weather);
+        recyclerView = findViewById(R.id.SearchWeatherRecyclerView);
+        citiesList = new ArrayList<>();
 
         setCitiesDefault();
+        setAdapter();
+    }
+
+    private void setAdapter() {
+        SearchCityRecyclerAdapter adapter = new SearchCityRecyclerAdapter(this, citiesList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
     }
 
     private void setCitiesDefault(){
