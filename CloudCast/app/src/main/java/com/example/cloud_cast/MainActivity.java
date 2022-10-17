41114c388a74bcb11557da41c645db0d28811316
omamoham <94276035+omamoham@users.noreply.github.com>
2022-10-17 04:55:17 -0500

    Created a home page Without any backend functionality for now! Shall be updated in coming days. Looks cool. Will give the buttons functionality soon

--- CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
+++ CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -12,14 +14,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-
-        final Button placeHolderButton = (Button) findViewById(R.id.ChrisPlaceHolderButton);
-        placeHolderButton.setOnClickListener(v -> {
+        //final Button placeHolderButton = (Button) findViewById(R.id.ChrisPlaceHolderButton);
+        //placeHolderButton.setOnClickListener(v -> {
             // Code here executes on main thread after user presses button
-            Intent intent = new Intent(v.getContext(), SearchWeather.class);
-            startActivity(intent);
-        });
+            //Intent intent = new Intent(v.getContext(), SearchWeather.class);
+           // startActivity(intent);
+       // });
     }
 
 
