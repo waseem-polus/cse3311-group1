c40eb7b128a62c209a01f3d9e363ff92e9b1973b
omamoham <94276035+omamoham@users.noreply.github.com>
2022-10-17 04:58:16 -0500

    Created a home page Without any backend functionality for now! Shall be updated in coming days. Looks cool. Will give the buttons functionality soon 2nd attempt

--- CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
+++ CloudCast/app/src/main/java/com/example/cloud_cast/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             //Intent intent = new Intent(v.getContext(), SearchWeather.class);
            // startActivity(intent);
        // });
+        //omair
     }
 
 
